1678b22a47d4375da8389b5b90c8d1d4e5115c54
RealPeha <real.peha@gmail.com>
2021-06-27 00:29:26 +0300

    update Migrator contract

--- polygon/Migrator.sol
+++ polygon/Migrator.sol
@@ -6,31 +6,23 @@ interface IERC20 {
 
 contract Migrator {
     address public owner;
-    address public admin;
     mapping (address => bool) public migrated;
     
     IERC20 private token;
 
     constructor(IERC20 _token) {
         owner = msg.sender;
-        admin = msg.sender;
         token = _token;
     }
     
     function migrate(address from, address payable to, uint amount) external {
-        require(admin == msg.sender || owner == msg.sender, "-_-");
+        require(owner == msg.sender, "-_-");
         require(!migrated[from], "You have already migrated");
         
         migrated[from] = true;
         
         token.transfer(to, amount);
         to.transfer(3000000 gwei); // 0.003 MATIC
-    }
-    
-    function setAdmin(address account) external {
-        require(owner == msg.sender, "-_-");
-
-        admin = account;
     }
 
     function grabStuckTokens(IERC20 _token, uint amount) external {
